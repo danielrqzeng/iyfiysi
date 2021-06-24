50273b09d3cf5d0a3df4b027db74124176d854a6
riqiangzeng <riqiangzeng@tencent.com>
2021-06-24 17:01:00 +0800

    bugfix:build script

--- install.bat
+++ install.bat
@@ -3,6 +3,11 @@ echo ==========================================
 echo install iyfiysi and its dependency
 echo on
 
+rem BUILT_AT=2020-03-30 10:08:54
+set BUILT_AT=%date:~0,4%-%date:~5,2%-%date:~8,2% %time%
+set COMMIT_TAG=unknow
+for /F %%i in ('git rev-parse HEAD') do ( set COMMIT_TAG=%%i)
+
 @rem run me to install iyfiysi and its dependency
 go mod download
 
@@ -24,11 +29,12 @@ go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 @rem install iyfiysi & protoc-gen-iyfiysi
 %GOPATH%\bin\statik -src=template -f
 cd cmd\iyfiysi
-go install .
+go install -ldflags "-X 'iyfiysi/internal/iyfiysi.commit=%COMMIT_TAG%' -X 'iyfiysi/internal/iyfiysi.date=%BUILT_AT%'" .
 
 cd ..\protoc-gen-iyfiysi
-go install .
+go install -ldflags "-X 'iyfiysi/internal/iyfiysi.commit=%COMMIT_TAG%' -X 'iyfiysi/internal/iyfiysi.date=%BUILT_AT%'" .
 
+cd ..
 cd ..
 @echo off
 echo ==========================================
--- install.sh
+++ install.sh
@@ -7,8 +7,17 @@ go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 
-statik -src=template
-cd cmd/iyfiysi;go install .
+BUILT_AT=$(date "+%Y-%m-%d %H:%M:%S") #2020-03-30 10:08:54
+COMMIT_TAG=$(git rev-parse HEAD) #1c7caa847ce196f0668e01794d3cd773944f3127
+if [ ${#COMMIT_TAG} -eq 40 ];then
+    COMMIT_TAG=${COMMIT_TAG:0:8}
+else
+    COMMIT_TAG="unknow"
+fi
+
+statik -src=template -f
+cd cmd/iyfiysi;go install -ldflags "-X 'iyfiysi/internal/iyfiysi.commit=$COMMIT_TAG' -X 'iyfiysi/internal/iyfiysi.date=$BUILT_AT'" .
 cd -
-cd cmd/protoc-gen-iyfiysi;go install .
+
+cd cmd/protoc-gen-iyfiysi;go install -ldflags "-X 'iyfiysi/internal/iyfiysi.commit=$COMMIT_TAG' -X 'iyfiysi/internal/iyfiysi.date=$BUILT_AT'" .
 cd -
